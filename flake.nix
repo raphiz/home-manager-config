1a8a35b33b23aea0da1f59b972d31cc47a730abd
Raphael Zimmermann <mail@raphael.li>
2023-07-17 21:36:36 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Personal Home Manager configuration of raphiz";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    homeConfigurations."raphiz" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [./home.nix];
+    };
+  };
+}
