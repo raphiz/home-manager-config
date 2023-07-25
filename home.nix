18ef3baf0e537bab0c0ee8d5757b3a89aa05473b
Raphael Zimmermann <mail@raphael.li>
2023-07-25 10:40:05 +0200

    Add custom desktop files + icons

--- home.nix
+++ home.nix
@@ -14,22 +14,40 @@
     # '')
   ];
 
-  home.file = {
-    ".config/ulauncher/extensions.json".text = builtins.readFile ./ulauncher/extensions.json;
-    ".config/ulauncher/settings.json".text = builtins.readFile ./ulauncher/settings.json;
-    ".config/ulauncher/shortcuts.json".text = builtins.readFile ./ulauncher/shortcuts.json;
-    ".config/autostart/ulauncher.desktop".text = builtins.readFile ./ulauncher/ulauncher.desktop;
+  home.file = let
+    applications = builtins.attrNames (builtins.readDir ./applications);
+    desktop-files = builtins.listToAttrs (
+      map (name: {
+        name = ".local/share/applications/${name}.desktop";
+        value = {source = ./applications/${name}/${name}.desktop;};
+      })
+      applications
+    );
+    icons = builtins.listToAttrs (
+      map (name: {
+        name = ".local/share/icons/hicolor/128x128/apps/${name}.png";
+        value = {source = ./applications/${name}/${name}.png;};
+      })
+      applications
+    );
+    files = {
+      ".config/ulauncher/extensions.json".text = builtins.readFile ./ulauncher/extensions.json;
+      ".config/ulauncher/settings.json".text = builtins.readFile ./ulauncher/settings.json;
+      ".config/ulauncher/shortcuts.json".text = builtins.readFile ./ulauncher/shortcuts.json;
+      ".config/autostart/ulauncher.desktop".text = builtins.readFile ./ulauncher/ulauncher.desktop;
 
-    ".config/libreoffice/4/user/wordbook/standard.dic".text = builtins.readFile ./libreoffice/standard.dic;
+      ".config/libreoffice/4/user/wordbook/standard.dic".text = builtins.readFile ./libreoffice/standard.dic;
 
-    ".gitconfig".text = builtins.readFile ./git/.gitconfig;
-    ".gitmessage".text = builtins.readFile ./git/.gitmessage;
-    ".gitignore".text = builtins.readFile ./git/.gitignore;
+      ".gitconfig".text = builtins.readFile ./git/.gitconfig;
+      ".gitmessage".text = builtins.readFile ./git/.gitmessage;
+      ".gitignore".text = builtins.readFile ./git/.gitignore;
 
-    ".config/starship.toml".text = builtins.readFile ./starship.toml;
+      ".config/starship.toml".text = builtins.readFile ./starship.toml;
 
-    ".config/qpdfview/shortcuts.conf".text = builtins.readFile ./qpdfview/shortcuts.conf;
-  };
+      ".config/qpdfview/shortcuts.conf".text = builtins.readFile ./qpdfview/shortcuts.conf;
+    };
+  in
+    desktop-files // icons // files;
 
   home.sessionVariables = {
     # EDITOR = "emacs";
