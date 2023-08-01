e0b94fd7652e9fb2003c98f09e613d88ce6d299e
Raphael Zimmermann <mail@raphael.li>
2023-08-01 10:28:56 +0200

    Convert plain gitconfig to nix

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -4,13 +4,75 @@
   pkgs,
   ...
 }: {
-  home.packages = [
-    pkgs.delta
-  ];
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+
+    userName = "Raphael Zimmermann";
+    userEmail = lib.mkDefault "mail@raphael.li";
+
+    aliases = {
+      a = "add --patch";
+      st = "status";
+      co = "commit";
+      unstage = "reset";
+      l = "log --oneline --decorate";
+      amend = "commit --amend --no-edit";
+      uncommit = "reset --soft HEAD~1";
+      sla = "log --oneline --decorate --graph --all";
+      new = "l main..HEAD";
+      missing = "l HEAD..main";
+    };
+
+    ignores = [
+      ".idea/codeStyles/Project.xml"
+    ];
+    includes = [
+      {path = "~/.gitlocal";}
+    ];
+    iniContent = {
+      core = {
+        whitespace = "warn";
+        autocrlf = "input";
+      };
+      commit = {
+        template = "~/.config/git/gitmessage";
+      };
+      pull = {
+        ff = "only";
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      push = {
+        default = "current";
+        autoSetupRemote = "true";
+      };
+      rebase = {
+        autoSquash = "true";
+        autoStash = "true";
+      };
+      diff = {
+        colorMoved = "default";
+      };
+    };
+  };
+
+  programs.git.delta = {
+    enable = true;
+    options = {
+      # use n and N to move between diff sections
+      navigate = true;
+      # set to true if you're in a terminal w/ a light background color (e.g. the default macOS terminal)
+      light = true;
+      side-by-side = true;
+    };
+  };
 
   home.file = {
-    ".gitconfig".source = ./.gitconfig;
-    ".gitmessage".source = ./.gitmessage;
-    ".gitignore".source = ./.gitignore;
+    ".config/git/gitmessage".source = ./gitmessage;
   };
 }
