2ad730d4181f8234e5078fab71a26f1f79e57dce
Raphael Zimmermann <mail@raphael.li>
2023-08-01 11:20:36 +0200

    Use sha256sum for direvn layouts

--- modules/shell.nix
+++ modules/shell.nix
@@ -52,7 +52,7 @@
       direnv_layout_dir() {
           echo "''${direnv_layout_dirs[$PWD]:=$(
               echo -n "${config.xdg.cacheHome}"/direnv/layouts/
-              echo -n "$PWD" | shasum | cut -d ' ' -f 1
+              echo -n "$PWD" | sha256sum | cut -d ' ' -f 1
           )}"
       }
     '';
